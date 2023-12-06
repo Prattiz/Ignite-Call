fb90a709a4a28cfab47498350c86afb696cc0861
Thiago <prattithiago5@gmail.com>
2023-12-06 15:25:00 -0300

    UPDATE HOME 1.2: CREATE FORM, ADD ZOD, UPDATE HTML/CSS

--- src/pages/home/components/ClainUsernameForm/index.tsx
+++ src/pages/home/components/ClainUsernameForm/index.tsx
@@ -1,22 +1,53 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { Form } from "./styles";
+import { Button, TextInput, Text } from "@ignite-ui/react";
+import { Form, FormAnotation } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+
+const clainUsernameFormSchema = z.object({
+    username: z.string()
+    .min(4, {message: 'o usuário deve ter mais de 4 caracteres'})
+    .regex(/^([a-z\\-]+)$/i, {message: 'O usuário só pode ter letras ou hifens'})
+    .transform(username => username.toLowerCase()),
+});
+
+type ClainUsernameFormData = z.infer<typeof clainUsernameFormSchema>
 
 export function ClainUserNameForm(){
-    return(
-        <Form as='form'>
-            <TextInput
-            size='sm'
-            prefix="ignite.com/"
-            placeholder="seu-usuario"/>
+    const { register, handleSubmit, formState: { errors } } = useForm<ClainUsernameFormData>({
+        resolver: zodResolver(clainUsernameFormSchema)
+    })
 
-            <Button
-            size='sm'
-            type="submit"
-            >
-                Reservar Usuário
-                <ArrowRight/>
-            </Button>
-        </Form>
+    async function handlePreRegister(data: ClainUsernameFormData){
+        console.log(data)
+    }
+
+    return(
+        <>
+            <Form as='form' onSubmit={handleSubmit(handlePreRegister)}>
+                <TextInput
+                size='sm'
+                prefix="ignite.com/"
+                placeholder="seu-usuario"
+                {...register('username')}
+                />
+
+                <Button
+                size='sm'
+                type="submit"
+                >
+                    Reservar Usuário
+                    <ArrowRight/>
+                </Button>
+            </Form>
+
+            <FormAnotation>
+                <Text size='sm'>
+                    {errors.username ? errors.username.message : 'Digite o Nome de Usuário'}
+                </Text>
+            </FormAnotation>
+        </>
     )
 }
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,5 @@
+export function Register(){
+    return(
+        <h1>hello world</h1>
+    )
+}
