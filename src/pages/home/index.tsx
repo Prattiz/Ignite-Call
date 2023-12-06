abb0583bb9ec8acc093bb702e00f0fb5f3d40b88
Thiago <prattithiago5@gmail.com>
2023-12-06 13:31:19 -0300

    UPDATE HOME 1.0

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,26 @@
-import { Heading } from "@ignite-ui/react";
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Content, ImageContainer } from "./styles";
+
+import previewImg from "../../assets/Calendar.png";
+import Image from "next/image";
 
 
 export default function Home() {
   return(
-    <Heading as="h1" >hello world</Heading>
+    <Container>
+      <Content>
+        <Heading size='4xl'>Agendamento descomplicado</Heading>
+        <Text size='lg'>Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.</Text>
+      </Content>
+      <ImageContainer>
+        <Image 
+        src={previewImg}
+        height={400}
+        quality={100}
+        priority
+        alt="Calendário"
+        />
+      </ImageContainer>
+    </Container>
   ) 
 }
