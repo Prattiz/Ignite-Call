5e846c8b81f63742af199ccf0d97c108e0fc90be
Thiago <prattithiago5@gmail.com>
2023-12-06 12:41:35 -0300

    ADD DESIGN SYSTEM, CREATING GLOBAL STYLES AND ADD ESLINT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import { GlobalStyles } from '../styles/global'
+
+GlobalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
