d3010782164b621d0ddf692535fda1c6ad9976bf
Thiago <prattithiago5@gmail.com>
2024-01-02 16:14:53 -0300

    CONFIG ESLINT

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -12,4 +12,6 @@ declare module 'next-auth' {
   interface Session {
     user: User
   }
+  
 }
+
